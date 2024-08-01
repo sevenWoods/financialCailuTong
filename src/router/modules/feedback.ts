710f1d8ad20b584c2cea9952f97345728d69108c
七木 <dakucha@qq.com>
2024-08-01 14:16:38 +0800

    chore: 创建用户、图片、反馈管理菜单栏

---
+++ src/router/modules/feedback.ts
@@ -0,0 +1,27 @@
+/** 
+ *todo: 投诉建议
+ *@param: 
+ *@author: 七木
+ *@date: 2024/08/01
+ */
+export default {
+  path: "/feedback",
+  redirect: "/feedback/index",
+  meta: {
+    icon: "ep:cellphone",
+    title: "投诉管理",
+    rank: 10
+  },
+  children: [
+    {
+      path: "/feedback/index",
+      name: "feedback",
+      component: () => import("@/views/feedback/index.vue"),
+      meta: {
+        title: "反馈信息",
+        // 通过设置showParent为true显示父级
+        // showParent: true
+      }
+    }
+  ]
+};
