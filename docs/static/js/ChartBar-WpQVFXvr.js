7d44ae658660da2d19970ad418d8be86ca72e6bc
七木 <dakucha@qq.com>
2024-08-02 18:42:40 +0800

    test: 打包

--- docs/static/js/ChartBar-WpQVFXvr.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./ChartBar.vue_vue_type_script_setup_true_lang-BT_smr2t.js";import"./index-Bj26mlpg.js";export{m as default};
--- docs/static/js/updateAgree-DRKVz2BS.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./updateAgree.vue_vue_type_script_setup_true_lang-mEpgE-Gv.js";import"./index-Bj26mlpg.js";export{m as default};
