92a5481b78f48d46fdded593afee48abde1a845b
七木 <dakucha@qq.com>
2024-08-21 11:00:04 +0800

    style:  用户

--- mock/login.ts
+++ mock/login.ts
@@ -10,7 +10,7 @@ export default defineFakeRoute([
         return {
           success: true,
           data: {
-            avatar: "https://avatars.githubusercontent.com/u/44761321",
+            avatar: "",
             username: "admin",
             nickname: "七木",
             // 一个用户可能有多个角色
@@ -24,9 +24,9 @@ export default defineFakeRoute([
         return {
           success: true,
           data: {
-            avatar: "https://avatars.githubusercontent.com/u/52823142",
+            avatar: "",
             username: "common",
-            nickname: "小林",
+            nickname: "七木",
             roles: ["common"],
             accessToken: "eyJhbGciOiJIUzUxMiJ9.common",
             refreshToken: "eyJhbGciOiJIUzUxMiJ9.commonRefresh",
